ed35bc7b23544c06e08efffa07a2767a10b546b4
Igor Martins <igormartins16@gmail.com>
2021-09-26 14:29:02 -0400

    Usando repetições para compor componentes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import "./App.css";
 
+import ListaAlunos from "./repeticao/ListaAlunos.";
 import Familia from "./components/basicos/Familia";
 import FamiliaMembro from "./components/basicos/FamiliaMembro";
 import Card from "./components/layout/Card";
@@ -15,6 +16,10 @@ export default (_) => (
     <h1>Fundamentos React</h1>
 
     <div className="Cards">
+      <Card titulo="#06 - Repetição" color="#FF4C65">
+        <ListaAlunos />
+      </Card>
+
       <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
         <Familia sobrenome="Ferreira">
           <FamiliaMembro nome="Pedro" />
